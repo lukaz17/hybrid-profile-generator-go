629dea41fe410983e1e34a62e08f30c89b026d36
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2025-06-07 13:58:04 +0700

    feat(video): define resolution struct

---
+++ video/readme.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2025 Nguyen Nhat Tung
+//
+// Hybrid Profile Generator is licensed under the MIT license.
+// You should receive a copy of MIT along with this software.
+// If not, see <https://opensource.org/license/mit>
+
+/*
+Package video defines properties for video files.
+*/
+
+package video
---
+++ video/resolution.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2025 Nguyen Nhat Tung
+//
+// Hybrid Profile Generator is licensed under the MIT license.
+// You should receive a copy of MIT along with this software.
+// If not, see <https://opensource.org/license/mit>
+
+package video
+
+// Resolution defines a video resolution with width, height, and frame rate.
+type Resolution struct {
+	Width     uint16
+	Height    uint16
+	FrameRate float64
+}
